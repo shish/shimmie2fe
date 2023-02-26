1b0b0b812bfff6890ae67f442b4dc0d6dbb773df
Shish <shish@shishnet.org>
2023-02-26 18:01:21 +0000

    have post list link to posts with search param set

--- src/pages/PostList/ThumbnailGrid/index.tsx
+++ src/pages/PostList/ThumbnailGrid/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { absurl } from "../../../utils";
 
 import css from "./ThumbnailGrid.module.scss";
 
 function Thumbnail({ post }: { post: any }) {
+    const [searchParams] = useSearchParams();
+    let link = "/post/" + post.post_id;
+    if(searchParams.get("tags")) {
+        link += "?tags=" + searchParams.get("tags");
+    }
     return (
-        <Link className={css.thumbnail} to={"/post/" + post.post_id}>
+        <Link className={css.thumbnail} to={link}>
             <img
                 alt={post.tooltip}
                 title={post.tooltip}
                 src={absurl(post.thumb_link)}
+                // FIXME: set aspect ratio?
                 // width={post!.width}
                 // height={post!.height}
             />
